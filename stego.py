495c768b805ee6805bd1fc98e26cc287c68eb930
Chris Tralie <chris.tralie@gmail.com>
2022-09-13 21:38:26 -0400

    Adding code to viterbi trace better target paths, and tweaking least squares objective function slightly

--- stego.py
+++ stego.py
@@ -3,6 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import cvxpy as cp
 import time
+from numba import jit
 from scipy import sparse
 from scipy.sparse import linalg as slinalg
 from scipy.signal import medfilt
@@ -192,6 +193,98 @@ def get_normalized_target(cent_orig, target, min_freq, max_freq, stdev=2):
     target /= np.max(target)
     return xmin + target*(xmax-xmin)
 
+
+@jit(nopython=True)
+def viterbi_loop_trace(csm, K):
+    """
+    Trace through a cyclic set of target states to best match the L1
+    norm between target states and time points
+
+    Parameters
+    ----------
+    csm: ndarray(M, N)
+        L1 Cross-similarity between M target states and N time points
+    K: int
+        Maximum jump interval between states
+    
+    Returns
+    -------
+    list(N)
+        State indices of best fit cyclic path
+    """
+    M = csm.shape[0]
+    N = csm.shape[1]
+    S = np.zeros((M, N))
+    S[:, 0] = csm[:, 0]
+    B = np.zeros((M, N)) # Backpointers
+    for j in range(1, N):
+        for i in range(M):
+            idxmin = i-K
+            valmin = np.inf
+            for k in range(i-K, i):
+                k = k%M
+                if S[k, j-1] < valmin:
+                    valmin = S[k, j-1]
+                    idxmin = k
+            S[i, j] = valmin + csm[i, j]
+            B[i, j] = idxmin
+    j = N-1
+    i = np.argmin(S[:, -1])
+    path = []
+    while j > 0:
+        path.append(i)
+        i = int(B[i, j])
+        j -= 1
+    path.append(i)
+    path.reverse()
+    return path
+
+def get_best_target(X, Y, K):
+    """
+    Return the best path through a target, traversing it in either direction
+
+    Parameters
+    ----------
+    X: ndarray(M, 2)
+        Target states
+    Y: ndarray(N, 2)
+        Time points
+    K: int
+        Maximum jump interval between states
+    """
+    K = 10 # Can jump by up to this number of points
+    costfn = lambda Z: np.sum(np.abs(Z-Y))
+
+    min_path = np.arange(Y.shape[0]) % X.shape[0]
+    min_cost = costfn(X[min_path, :])
+    print("Default cost", min_cost)
+
+    # Try default orientation
+    csm = np.abs(X[:, 0][:, None] - Y[:, 0][None, :])
+    csm += np.abs(X[:, 1][:, None] - Y[:, 1][None, :])
+    path = viterbi_loop_trace(csm, K)
+    path = np.array(path, dtype=int)
+    cost = costfn(X[path, :])
+    print("Cost", cost)
+    if cost < min_cost:
+        min_cost = cost
+        min_path = path
+    
+    # Try reverse orientation
+    csm = np.abs(X[:, 0][:, None] - Y[::-1, 0][None, :])
+    csm += np.abs(X[:, 1][:, None] - Y[::-1, 1][None, :])
+    path = viterbi_loop_trace(csm, K)
+    path = X.shape[0]-1-np.array(path, dtype=int)
+    cost = costfn(X[path, :])
+    print("Reverse cost", cost)
+    if cost < min_cost:
+        print("Reverse wins!")
+        min_cost = cost
+        min_path = path
+
+    return min_path, min_cost
+
+
 def windowed_spec_centroid_perturb(Mag, target, win, min_freq, max_freq, eps_add, eps_ratio=0, Mag_fixed=np.array([])):
     """
     Perturb a sliding window of the spectral centroid of a magnitude spectrogram within 
@@ -230,21 +323,26 @@ def windowed_spec_centroid_perturb(Mag, target, win, min_freq, max_freq, eps_add
     M = max_freq - min_freq
     N = Mag.shape[1]
     assert(target.size == N-win+1)
-    An, Ad = get_windowed_spec_centroid_matrices(Mag, min_freq, max_freq, win)
+    An, _ = get_windowed_spec_centroid_matrices(Mag, min_freq, max_freq, win)
+    _, Ad = get_windowed_spec_centroid_matrices(Mag, min_freq, max_freq, 1)
+    A = sparse.vstack([An, Ad])
+    A = A.tocsr()
+    print("Sparsity factor:", A.count_nonzero()/(A.shape[0]*A.shape[1]))
+
     x_orig = (Mag[min_freq:max_freq, :].T).flatten()
+    b = np.concatenate((target.flatten(), Ad.dot(x_orig).flatten()))
 
     ## Step 2: Solve the optimization problem and return a spectrogram
     ## with the result
     x = cp.Variable(N*M)
-    objective = cp.Minimize(cp.sum_squares(An@x - target))
-    constraints = [Ad@x == Ad.dot(x_orig)]
+    objective = cp.Minimize(cp.sum_squares(A@x - b))
     xmin = x_orig - eps_add
     xmax = x_orig + eps_add
     if eps_ratio > 0:
         xmin = np.minimum(xmin, x_orig*(1-eps_ratio))
         xmax = np.maximum(xmax, x_orig*(1+eps_ratio))
     xmin[xmin < 0] = 0
-    constraints += [xmin <= x, x <= xmax]
+    constraints = [xmin <= x, x <= xmax]
     if Mag_fixed.size > 0:
         m = (Mag_fixed[min_freq:max_freq, :].T).flatten()
         constraints += [x[0:Mag_fixed.size] == m]
