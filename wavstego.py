c45ac7845bca8e0511be9a5c56425fc30af815ff
Chris Tralie <chris.tralie@gmail.com>
2022-10-13 21:32:23 -0400

    Switching sliding window scheme

--- wavstego.py
+++ wavstego.py
@@ -4,51 +4,59 @@ import numpy as np
 from scipy.sparse.linalg import LinearOperator
 from scipy.optimize import lsq_linear
 from pywt import wavedec, waverec, dwt, idwt
+import cvxpy as cp
 from stego import *
 import time
-
 
 class SlidingWindowCentroidMatrix(LinearOperator):
     """
     Perform the effect of a sliding window average between two
     parallel arrays
     """
-    def __init__(self, x_orig, win, lam=1):
+    def __init__(self, x_orig, win, denom_lam=1):
         """
         Parameters
         ----------
-        x_orig: ndarray(2, N)
-            The two signals
+        x_orig: ndarray(K, N)
+            Two sets of signals between which to compute the average
         win: int
             Window length to use
-        lam: float
-            The weight to put on fit to sliding window sum
+        denom_lam: float
+            The weight to put on the denominator
         """
+        K = x_orig.shape[0]
         N = x_orig.shape[1]
         M = N-win+1
+        self.K = K
         self.N = N
-        self.shape = ((2*M, 2*N))
+        self.shape = ((2*M, K*N))
         self.win = win
-        self.lam = lam
+        self.denom_lam = denom_lam
         self.dtype = float
         x_orig = np.sum(x_orig, axis=0)
         x_orig = np.concatenate(([0], x_orig))
         x_orig = np.cumsum(x_orig)
         self.denom = x_orig[win::]-x_orig[0:-win]
+        self.mul_calls = 0
+        self.rmul_calls = 0
             
     
-    def _matvec(self, x):
+    def _matvec(self, x_param):
         """
         y1 holds sliding window sum
         y2 holds weighted sliding window sum
         """
-        x = np.reshape(x, (2, x.size//2))
-        x = np.concatenate(([[0], [0]], x), axis=1)
+        K = self.K
+        self.mul_calls += 1
+        self.rmul_calls += 1
+        x = np.zeros((K, x_param.size//K + 1))
+        x[:, 1::] = np.reshape(x_param, (K, x_param.size//K))
         y1 = np.sum(x, axis=0)
         y1 = np.cumsum(y1)
         y1 = y1[self.win::]-y1[0:-self.win]
-        y1 = 1.5*self.lam*y1/self.denom
-        mul = np.array([[1], [2]])
+        y1 = 1.5*self.denom_lam*y1/self.denom
+        mul = np.ones((K, 1))
+        mul[K//2::] = 2
         x2 = np.sum(x*mul, axis=0)
         y2 = np.cumsum(x2)
         y2 = y2[self.win::]-y2[0:-self.win]
@@ -63,25 +71,19 @@ class SlidingWindowCentroidMatrix(LinearOperator):
         p[self.win:self.win+y.size] = y*fac
         p = np.cumsum(p)
         return p[self.win::] - p[0:-self.win]
-        
     
     def _rmatvec(self, y):
+        K = self.K
         N = self.N
         y1 = y[0:y.size//2]
         y2 = y[y.size//2::]
-        x = np.zeros(2*N)
-        # for i, yi in enumerate(y[0:y.size//2]):
-        #     x[0, i:i+self.win] += 1.5*self.lam*yi/self.denom[i]
-        #     x[1, i:i+self.win] += 1.5*self.lam*yi/self.denom[i]
-        p1 = self.get_sliding_window_contrib(y1, 1.5*self.lam/self.denom)
-        x[0:N] += p1
-        x[N::] += p1
-        #for i, yi in enumerate(y[y.size//2::]):
-        #    x[0, i:i+self.win] += yi/self.denom[i]
-        #    x[1, i:i+self.win] += 2*yi/self.denom[i]
-        x[0:N] += self.get_sliding_window_contrib(y2, 1/self.denom)
-        x[N::] += self.get_sliding_window_contrib(y2, 2/self.denom)
-        return x
+        x = np.zeros((K, N))
+        p1 = self.get_sliding_window_contrib(y1, 1.5*self.denom_lam/self.denom)
+        x += p1[None, :]
+        x[0::K//2, :] += self.get_sliding_window_contrib(y2, 1/self.denom)[None, :]
+        x[K//2::, :] += self.get_sliding_window_contrib(y2, 2/self.denom)[None, :]
+        return x.flatten()
+
 
 def subdivide_wavlevel_dec(x, max_depth, wavtype, depth=1):
     res = []
@@ -106,17 +108,20 @@ def subdivide_wavelet_rec(coeffs, wavtype):
 
 
 class WaveletCoeffs:
-    def __init__(self, x, target, win, lam=1, k=2, viterbi_K=4, wavtype='haar', wavlevel=4):
+    def __init__(self, x, target, win, fit_var=0.5, denom_lam=1, k=2, viterbi_K=4, wavtype='haar', wavlevel=4):
         """
         Parameters
         ----------
         x: ndarray(N pow of 2)
             Audio samples
-        target: ndarray(M, 2)
+        target: ndarray(M, dim)
             Target curve
         win: int
             Window length to use
-        lam: float
+        fit_var: float
+            A variance proportion of the fit.  0 means very little variance, 1 means
+            the maximum variance
+        denom_lam: float
             The weight to put on fit to sliding window sum
         k: int
             Index of first wavelet to split up
@@ -133,9 +138,11 @@ class WaveletCoeffs:
         coeffs = [[c] for c in coeffs[::-1]]
         self.coeffs = coeffs
         self.k = k
+        self.dim = target.shape[1]
         self.wavtype = wavtype
         self.win = win
-        self.lam = lam
+        self.fit_var = fit_var
+        self.denom_lam = denom_lam
         
         divamt = 1+int(np.round(np.log2(coeffs[k][0].size/(target.shape[0]+win))))
         coeffs[k] = subdivide_wavlevel_dec(coeffs[k][0], divamt, wavtype)
@@ -145,25 +152,21 @@ class WaveletCoeffs:
         ## Step 2: Setup all aspects of sliding windows
         self.signs = [np.sign(x) for x in coeffs[k] + coeffs[k+1]] # Signs of wavelet coefficients before squaring
         self.pairs_sqr = [p**2 for p in coeffs[k] + coeffs[k+1]] # Squared wavelet coefficients
-        self.mats = [] # Matrices to do sliding window averaging transforms on each pair
-        self.targets = [] # Normalized target time series for each pair
-        self.coords = [] # Coordinates of each pair
+        print("len(self.pairs_sqr)", len(self.pairs_sqr))
+        self.mats = [] # Matrices to do sliding window averaging transforms on each group of coefficients (dim total)
+        self.targets = [] # Normalized target time series for each coordinate (dim total)
+        self.coords = [] # Coordinate indices of each group of coefficients
         csm = np.array([]) # Cross-similarity matrix for aligned targets
-        for i in range(0, len(self.signs), 2):
-            Y = np.array([self.pairs_sqr[i], self.pairs_sqr[i+1]])
-            Mat = SlidingWindowCentroidMatrix(Y, win, lam)
+        K = len(self.signs)//self.dim
+        for coord in range(self.dim):
+            self.coords += [coord]*K
+            Y = np.array(self.pairs_sqr[coord*K:(coord+1)*K])
+            Mat = SlidingWindowCentroidMatrix(Y, win, denom_lam)
             self.mats.append(Mat)
             res = Mat.dot(Y.flatten())
             res = res[res.size//2::]
             targeti = []
-            if i < len(self.signs)//2:
-                # First half of pairs are the x coordinate
-                targeti = np.array(target[:, 0])
-                self.coords.append(0)
-            else:
-                # Second half of pairs are the y coordinate
-                targeti = np.array(target[:, 1])
-                self.coords.append(1)
+            targeti = np.array(target[:, coord])
             targeti = get_normalized_target(res, targeti, 1, 2)
             self.targets.append(targeti)
             csmi = np.abs(targeti[:, None] - res[None, :])
@@ -191,80 +194,32 @@ class WaveletCoeffs:
         to best match their targets
         """
         M = len(self.targets[0])
-        for i in range(0, len(self.targets)):
+        K = len(self.pairs_sqr)//self.dim
+        for coord in range(self.dim):
             tic = time.time()
-            print("Computing target {} of {}...\n".format(i+1, len(self.targets)))
-            y = 1.5*self.lam*np.ones(M*2)
-            y[M::] = self.targets[i]
-            mx = 1.1*max(np.max(self.pairs_sqr[i*2]), np.max(self.pairs_sqr[i*2+1]))
-            res = lsq_linear(self.mats[i], y, (0, mx))
-            self.pairs_sqr[i*2] = res[0:res.size//2]
-            self.pairs_sqr[i*2+1] = res[res.size//2::]
+            print("Computing target coordinate {} of {}...\n".format(coord+1, self.dim))
+            y = 1.5*self.denom_lam*np.ones(M*2)
+            y[M::] = self.targets[coord]
+
+            Y = np.array(self.pairs_sqr[coord*K:(coord+1)*K])
+            shape = Y.shape
+            Y = Y.flatten()
+            eps_add = np.quantile(Y, self.fit_var)
+            mn = np.maximum(0, Y-eps_add)
+            mx = Y+eps_add
+            res = lsq_linear(self.mats[coord], y, (mn, mx), verbose=2)['x']
+            """
+            x = cp.Variable(self.pairs_sqr[0].size)
+            objective = cp.Minimize(cp.sum_squares(self.mats[i] @ x - y))
+            constraints = [0 <= x, x <= mx]
+            prob = cp.Problem(objective, constraints)
+            prob.solve()
+            res = np.array(x.value)
+            """
+            res = np.reshape(res, shape)
+            for k in range(res.shape[0]):
+                self.pairs_sqr[coord*K+k] = res[k, :]
             print("Elapsed time: {:.3f}".format(time.time()-tic))
-
-
-    def get_avg(self, Xs, normalize=False):
-        """
-        Return the average of elements in an array
-
-        Parameters
-        ----------
-        Xs: list of ndarray(N)
-            Arrays to average
-        normalize: bool
-            Whether to z-normalize each array
-
-        Returns
-        -------
-        averaged
-        """
-        X = np.zeros_like(Xs[0])
-        for Xi in Xs:
-            if normalize:
-                Xi = (Xi-np.mean(Xi))/np.std(Xi)
-            X += Xi
-        return X / len(Xs)
-
-    def get_target_avg(self, normalize=False):
-        """
-        Compute the averaged of the z-normalized target components
-
-        Returns
-        -------
-        ndarray(M, 2)
-            The normalized target
-        """
-        M = self.targets[0].size
-        Y = np.zeros((M, 2))
-        for k in range(2):
-            Y[:, k] = self.get_avg([t for i, t in zip(self.coords, self.targets) if i == k], normalize)
-        return Y
-    
-    def get_signal_avg(self, normalize=False):
-        """
-        Compute the average of the sliding window centroids
-
-        Returns
-        -------
-        ndarray(M, 2)
-            Average of the sliding window centroids
-        """
-
-        k = len(np.unique(self.coords))
-        M = self.targets[0].size
-        X = np.zeros((M, k))
-        counts = np.zeros(k)
-        for i in range(0, len(self.pairs_sqr), 2):
-            Y = np.array([self.pairs_sqr[i], self.pairs_sqr[i+1]])
-            Mat = self.mats[i//2]
-            res = Mat.dot(Y.flatten())
-            res = res[res.size//2::]
-            k = self.coords[i//2]
-            if normalize:
-                res = (res-np.mean(res))/np.std(res)
-            X[:, k] += res
-            counts[k] += 1
-        return X/counts[None, :]
     
     def reconstruct_signal(self):
         """
@@ -281,20 +236,46 @@ class WaveletCoeffs:
         coeffs[self.k+1] = [subdivide_wavelet_rec(coeffs2, self.wavtype)]
         return waverec([c[0] for c in coeffs[::-1]], self.wavtype)
     
+    def get_target(self, normalize=False):
+        ret = np.array(self.targets).T
+        if normalize:
+            ret = (ret - np.mean(ret, axis=0)[None, :])
+            ret = ret/np.std(ret, axis=0)[None, :]
+        return ret
+
+    def get_signal(self, normalize=False):
+        """
+        Compute and z-normalize the sliding window centroids
+
+        Returns
+        -------
+        ndarray(M, dim)
+            Average of the sliding window centroids
+        """
+        M = self.targets[0].size
+        K = len(self.pairs_sqr)//self.dim
+        X = np.zeros((M, self.dim))
+        for coord in range(self.dim):
+            Y = np.array(self.pairs_sqr[coord*K:(coord+1)*K])
+            Mat = SlidingWindowCentroidMatrix(Y, self.win, self.denom_lam)
+            self.mats.append(Mat)
+            res = Mat.dot(Y.flatten())
+            x = res[res.size//2::]
+            if normalize:
+                x = (x-np.mean(x))/np.std(x)
+            X[:, coord] = x
+        return X
+
     def plot(self, normalize=False):
-        Y = self.get_target_avg(normalize)
-        Z = self.get_signal_avg(normalize)
-        plt.figure(figsize=(12, 8))
-        plt.subplot(211)
-        plt.plot(Y[:, 0])
-        plt.plot(Z[:, 0])
-        plt.title("X Coordinate")
-        plt.legend(["Target", "Signal"])
-        plt.subplot(212)
-        plt.plot(Y[:, 1])
-        plt.plot(Z[:, 1])
-        plt.title("Y Coordinate")
-        plt.legend(["Target", "Signal"])
+        Y = self.get_target(normalize)
+        Z = self.get_signal(normalize)
+        res = 4
+        plt.figure(figsize=(res*3, res*self.dim))
+        for k in range(self.dim):
+            plt.subplot(self.dim, 1, k+1)
+            plt.plot(Y[:, k])
+            plt.plot(Z[:, k])
+            plt.legend(["Target", "Signal"])
 
         plt.figure(figsize=(6, 6))
         plt.plot(Y[:, 0], Y[:, 1])
