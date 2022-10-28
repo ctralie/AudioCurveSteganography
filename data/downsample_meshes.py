447b1a9a0dde401fd1b58684e4642731bb7259c1
Chris Tralie <chris.tralie@gmail.com>
2022-10-28 00:26:04 -0400

    Revert "Downsample meshes batch script"
    This reverts commit f57488ef89d896fe57221b57f683d48bbb9299f8.

--- data/downsample_meshes.py
+++
@@ -1,7 +0,0 @@
-import glob
-import subprocess
-
-for filein in glob.glob("meshseg_orig/*.off"):
-    print(filein)
-    fileout = "meshseg/{}".format(filein.split("/")[1])
-    subprocess.call(["meshlabserver", "-i", filein, "-o", fileout, "-s", "simplify-mesh.mlx"])
